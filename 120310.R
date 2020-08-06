8a0c5bfc66d4999949f1c8f0a4628f18852b955d
Fernando Elias de Oliveira Reis <feoreis.reis@gmail.com>
2020-08-06 18:26:22 -0300

    Add files via upload

---
+++ 120310.R
@@ -0,0 +1,9 @@
+
+#______Download And Run Code ______#
+URL      <- "http://rfunction.com/code/1202/120222.R"
+destfile <- "downloaded/120222.R"
+download.file(URL, destfile)
+source(destfile)
+ls()
+
+titles[1]
